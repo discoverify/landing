2cf64a0397a135b6ca30de46d005a1c95b3b95d7
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-11-18 23:40:02 +0100

    Add simple favicon

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Discoverify.app</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Discover songs you'll love" />
+      <link rel="icon" href="/favicon.ico" />
     </>
   );
 }
